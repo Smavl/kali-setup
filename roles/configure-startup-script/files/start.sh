d8730c3b7b7a422e8d18bf18e2b26153d57b5255
smavl <donsmavl@protonmail.com>
2025-05-20 15:56:54 +0200

    keybinds for gnome + some tools

--- roles/configure-startup-script/files/start.sh
+++ roles/configure-startup-script/files/start.sh
@@ -1,4 +1,16 @@
-setxkbmap dk
+setxkbmap -layout dk -variant nodeadkeys
 
 xmodmap -e "clear lock"
 xmodmap -e "keycode 66 = Escape"
+
+# === gnome things ===
+# Alt tabing
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab', '<Super>Tab']"
+# workspace navigation
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>Right']"
+# move to workspaces
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super><Shift>Page_Up', '<Super><Shift><Alt>Left', '<Control><Shift><Alt>Left', '<Shift><Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super><Shift>Page_Down', '<Super><Shift><Alt>Right', '<Control><Shift><Alt>Right', '<Shift><Alt>Right']"
+
